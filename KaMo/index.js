8803785830e9e5d2c7fa86bda27dc8ff16a251bc
2VZdtw8 <83011045+2VZdtw8@users.noreply.github.com>
2021-08-21 17:29:39 +0300

    Add files via upload

--- KaMo/index.js
+++ KaMo/index.js
@@ -1,9 +1,10 @@
 function collapse(){
-    var navLinks = document.querySelector("#nav-links").className;
-    if(navLinks === "nav-links"){
-        navLinks = "nav-links-expand";
-    }else if(navLinks === "nav-links-expand"){
-        navLinks = "nav-links";
+    var navLinks = $("#nav-links");
+    if(navLinks.attr("class") === "nav-links"){
+        $("#nav-links").removeClass("nav-links");
+        $("#nav-links").addClass("nav-links-expand");
+    }else if(navLinks.attr("class") === "nav-links-expand"){
+        $("#nav-links").removeClass("nav-links-expand");
+        $("#nav-links").addClass("nav-links");
     }
-    console.log(navLinks);
 }
